0724244d226024bfc941e9536b42c90de4c840fa
Nader Kanounji <naderkanounji@gmail.com>
2021-12-28 16:08:11 +0200

    initial commit

---
+++ cards/lib/cards.ex
@@ -0,0 +1,14 @@
+defmodule Cards do
+  @doc """
+  Create Deck.
+
+  ## Examples
+
+      iex> Cards.create_deck()
+      [deck]
+
+  """
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+end
